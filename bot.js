09646b12c4bfdb5810e07a50d61cfb7497758b4c
Uzair Ahmed <uzair318@vt.edu>
2019-06-02 18:26:50 -0400

    Modified regex

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
-function respond() {//**** */
+function respond() {//* */
   var request = JSON.parse(this.req.chunks[0]),
       //botRegex = /^\/cool guy$/;
-      botRegex = /^verdict?$/;
+      botRegex = /^verdict\?$/g;
 
   if(request.text && botRegex.test(request.text)) { //text coming in, Regex (regular expressions)
     this.res.writeHead(200);
@@ -61,6 +61,9 @@ function postMessage() {
   });
   botReq.end(JSON.stringify(body));
 }
+
+
+
 function postMessage2() {
   var botResponse, options, body, botReq;
 
