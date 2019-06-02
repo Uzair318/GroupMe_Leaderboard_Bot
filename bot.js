838eec4912d1edd8739c43d7334633e0fc50873e
Uzair Ahmed <uzair318@vt.edu>
2019-06-02 16:12:25 -0400

    Uncommented code in postMessage() and added it to postMessage2()

--- bot.js
+++ bot.js
@@ -43,21 +43,21 @@ function postMessage() {
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
-  // botReq = HTTPS.request(options, function(res) {
-  //     if(res.statusCode == 202) {
-  //       //neat
-  //     } else {
-  //       console.log('rejecting bad status code ' + res.statusCode);
-  //     }
-  // });
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
 
-  // botReq.on('error', function(err) {
-  //   console.log('error posting message '  + JSON.stringify(err));
-  // });
-  // botReq.on('timeout', function(err) {
-  //   console.log('timeout posting message '  + JSON.stringify(err));
-  // });
-  // botReq.end(JSON.stringify(body));
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 function postMessage2() {
   var botResponse, options, body, botReq;
@@ -78,6 +78,21 @@ function postMessage2() {
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
+  botReq = HTTPS.request(options, function(res) {
+    if(res.statusCode == 202) {
+      //neat
+    } else {
+      console.log('rejecting bad status code ' + res.statusCode);
+    }
+});
+
+botReq.on('error', function(err) {
+  console.log('error posting message '  + JSON.stringify(err));
+});
+botReq.on('timeout', function(err) {
+  console.log('timeout posting message '  + JSON.stringify(err));
+});
+botReq.end(JSON.stringify(body));
 
 }
 exports.respond = respond;
