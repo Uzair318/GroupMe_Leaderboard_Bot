9e43b71ffc205cae049ae7bd3c9402a9f9f3a9fc
Uzair Ahmed <uzair318@vt.edu>
2019-06-20 21:52:36 -0400

    added code to get group message data

--- bot.js
+++ bot.js
@@ -3,10 +3,35 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
+// https://dev.groupme.com/docs/v3 ~ API documentation
+const baseUrl = 'https://api.groupme.com/v3/groups/';
+const token = 'token=1df9001037c901372aca3263649c7787';
+const groupId = '50769460';
+const msgLimit = '100';
+
+// GET /groups/:group_id/messages
+const url = baseUrl + groupId + '/messages' + '?' + token + '&limit=' + msgLimit;
+
+// Library used for HTTP Requests
+const axios = require('axios');
+
+// same thing as curl
+axios.get(url)
+  .then(function (response) {
+    // gets response and prints messages
+    const messages = response.data.response.messages;
+    console.log(JSON.stringify(messages, '', 2));
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
 function respond() {//* */
   var request = JSON.parse(this.req.chunks[0]),
       //botRegex = /^\/cool guy$/;
-      botRegex = /^verdict\?$/i;
+      botRegex = /^verdict\?$/i;  // i flag -> case insensitive string
 
   if(request.text && botRegex.test(request.text)) { //text coming in, Regex (regular expressions)
     this.res.writeHead(200);
