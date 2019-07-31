05d03d57d57f4fda8ef16cd7d986b40310c07685
Uzair Ahmed <uzair318@vt.edu>
2019-07-31 17:15:03 -0400

    added Admin permissions function. need to test

--- bot.js
+++ bot.js
@@ -195,11 +195,11 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) { //text coming in, Regex (regular expressions)
     this.res.writeHead(200);
-    postMessage(senderID);
+    postMessage();
     this.res.end();
   } else if(request.text && (request.text == "/postResults")) {
     this.res.writeHead(200);
-    postResults();
+    postResults(senderID);
     this.res.end();
   } 
   else {
@@ -211,11 +211,10 @@ function respond() {
 
 
 
-function postMessage(senderID) {
+function postMessage() {
   var botResponse, options, body, botReq;
 
-  //addition for debugging purposes
-  botResponse = "Guilty!" + "\n" + "senderID = " + senderID; //Should be in string form
+  botResponse = "Guilty!"; //Should be in string form
 
   options = {
     hostname: 'api.groupme.com',
@@ -251,10 +250,10 @@ function postMessage(senderID) {
 
 
 
-function postResults() {
+function postResults(senderID) {
   var botResponse, options, body, botReq;
-
-  responseString = createOutput()
+  if(Admins.includes(senderID)) {
+    responseString = createOutput()
     .then(responseString => {
       botResponse = responseString; //Should be in string form
 
@@ -292,6 +291,42 @@ function postResults() {
         botReq.end(JSON.stringify(body));
       }
     )}
+    else { //not allowed
+      botResponse = "Sorry, you do not have permission to do this."; //Should be in string form
+
+        options = {
+          hostname: 'api.groupme.com',
+          path: '/v3/bots/post',
+          method: 'POST'
+        };
+
+        body = {
+          "bot_id" : botID,
+          "text" : botResponse
+        };
+
+
+        console.log('sending ' + botResponse + ' to ' + botID);
+
+        botReq = HTTPS.request(options, function(res) {
+            if(res.statusCode == 202) {
+              //neat
+            } else {
+              console.log('rejecting bad status code ' + res.statusCode);
+            }
+        });
+
+        botReq.on('error', function(err) {
+          console.log('error posting message '  + JSON.stringify(err));
+        });
+        botReq.on('timeout', function(err) {
+          console.log('timeout posting message '  + JSON.stringify(err));
+        });
+        botReq.end(JSON.stringify(body));
+      }
+    }
+ 
+
 
   
   
