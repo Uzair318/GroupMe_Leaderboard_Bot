f7c060da270d7c488765913131ecd3755dda34ad
Uzair Ahmed <uzair318@vt.edu>
2019-08-30 17:55:45 -0400

    syntax

--- bot.js
+++ bot.js
@@ -281,48 +281,46 @@ function postResults(senderID) {
     highestObj = mongo.highestToString()
 
     Promise.all([responseString, highestObj]).then((resultArray) => {
-        botResponse = resultArray[0]; //Should be in string form
-        botResponse += "\n" + resultArray[1].result;
+      botResponse = resultArray[0]; //Should be in string form
+      botResponse += "\n" + resultArray[1].result;
 
-            botResponse += "\n" + highestString.result;
+      options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+      };
 
-            options = {
-              hostname: 'api.groupme.com',
-              path: '/v3/bots/post',
-              method: 'POST'
-            };
-
-            body = {
-              "bot_id": botID,
-              "text": botResponse,
-              "attachments": [
-                {
-                  "type": "image",
-                  "url": resultArray[1].imgURL
-                }
-              ]
-            };
-
-
-            console.log('sending ' + botResponse + ' to ' + botID);
-
-            botReq = HTTPS.request(options, function (res) {
-              if (res.statusCode == 202) {
-                //neat
-              } else {
-                console.log('rejecting bad status code ' + res.statusCode);
-              }
-            });
-
-            botReq.on('error', function (err) {
-              console.log('error posting message ' + JSON.stringify(err));
-            });
-            botReq.on('timeout', function (err) {
-              console.log('timeout posting message ' + JSON.stringify(err));
-            });
-            botReq.end(JSON.stringify(body));
+      body = {
+        "bot_id": botID,
+        "text": botResponse,
+        "attachments": [
+          {
+            "type": "image",
+            "url": resultArray[1].imgURL
           }
-          )
+        ]
+      };
+
+
+      console.log('sending ' + botResponse + ' to ' + botID);
+
+      botReq = HTTPS.request(options, function (res) {
+        if (res.statusCode == 202) {
+          //neat
+        } else {
+          console.log('rejecting bad status code ' + res.statusCode);
+        }
+      });
+
+      botReq.on('error', function (err) {
+        console.log('error posting message ' + JSON.stringify(err));
+      });
+      botReq.on('timeout', function (err) {
+        console.log('timeout posting message ' + JSON.stringify(err));
+      });
+      botReq.end(JSON.stringify(body));
+    }
+    )
 
   }
   else { //not allowed
